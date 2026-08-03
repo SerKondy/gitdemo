171ba24164b09ea18fe186b142995298aacfae5e
serkondy <sergeykakondusow@gmail.com>
2026-08-03 13:57:15 -0700

    commit from javarush

--- src/Greeting.java
+++ src/Greeting.java
@@ -2,6 +2,6 @@ package src;
 
 public class Greeting {
     public static void main(String[] args) {
-	System.out.println("Hello from new!!!");
+	System.out.println("Hello from old!!!");
     }
 }
--- src/hel.java
+++ src/hel.java
@@ -2,6 +2,6 @@ package src;
 
 public class hel {
     public static void main(String[] args) {
-	    System.out.println("Hello 2.0!!!");
+	    System.out.println("Hello 3.0!!!");
     }
 }
