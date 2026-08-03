d95aa9d030bc380ee18f5e9154592059d0a0baa6
serkondy <sergeykakondusow@gmail.com>
2026-08-03 13:01:19 -0700

    From 03.08.

--- hello.java
+++
@@ -1,5 +0,0 @@
-public class hello {
-    static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/hel.java
+++ src/hel.java
@@ -1,5 +1,7 @@
+package src;
+
 public class hel {
     public static void main(String[] args) {
-	System.out.println("New hello!!!");
+	    System.out.println("Hello 2.0!!!");
     }
 }
